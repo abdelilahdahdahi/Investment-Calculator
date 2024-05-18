faef5f0ffb5d557dd6b4fb7e03250b9833b7001f
abdelilah <dahdahiabdelilah2002@gmail.com>
2024-05-18 20:12:55 +0200

    convert string input value to number

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     setUserInput(prevUserInput => {
         return {
             ...prevUserInput,
-            [inputId]: newValue
+            [inputId]: +newValue
         }
     })
   }
